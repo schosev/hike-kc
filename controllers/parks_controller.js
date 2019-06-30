eb146871f122764520739d5539e1dd0d6c13adc1
Joe H <schosev@yahoo.com>
2019-06-29 23:10:40 -0500

    added parkMap logic

--- controllers/parks_controller.js
+++ controllers/parks_controller.js
@@ -15,21 +15,21 @@ module.exports = {
         },
         include: [{
           model: db.Trail,
-          where: {trail_id: req.params.parkId}
-          ,
+          // where: {fk_park_id: db.Park.park_id}
+          // ,
           include: [{
             model: db.Track,
-            where: {track_id: db.Trail.trail_id}
-          ,
+          //   where: {track_id: db.Trail.trail_id}
+          // ,
             include: [{
               model: db.Cord,
-              where: {cord_id: db.Track.cord_id}
+              // where: {cord_id: db.Track.cord_id}
             }],
       //       order: [[{model: db.Cord}, "track_id", "cord_id" ]] 
           }],
-      //     order: [[{ model: db.Track}, "trail_id", "track_id"]]
+          // order: [[{ model: db.Track}, "trail_id", "track_id"]]
         }],
-      //   order: [[{ model: db.Trail}, "park_id", "trail_id"]]
+        // order: [[{ model: db.Trail}, "park_id", "trail_id"]]
       })
       .then(dbPark => res.json(dbPark))
       .catch(err => res.status(422).json(err));
--- public/assets/js/parkMap.js
+++ public/assets/js/parkMap.js
@@ -11,36 +11,61 @@ var map, infoWindow;
         function trailMarkers() {
           var url = window.location.pathname;
           var id = url.substring(url.lastIndexOf('/') + 1);
+          var idNbr = parseInt(id);
           console.log("url id ", id);
 
-          // $.get("/api/onePark", function(dbPark, err) {
-          // console.log('Park ', dbPark);
-          // console.log("err", err);
+          $.get("/api/onePark/" + idNbr, function(dbPark, err) {
+          console.log('Park ', dbPark);
+          console.log("err", err);
 
-          // dbPark.forEach(function(markerElem) {
-          //   console.log("markElem " ,markerElem);
-          //   var parkId = markerElem.park_id.toString();
-          //   var name = markerElem.park_name;
-          //   var address = markerElem.park_address;
-          //   console.log("address ", address);
-          //   var rating = markerElem.park_rating;
-          //   var elev = markerElem.park_elev;
-          //   var point = new google.maps.LatLng(
-          //       parseFloat(markerElem.park_lat),
-          //       parseFloat(markerElem.park_lon));
-
-          //   var marker = new google.maps.Marker({
-          //     map: map,
-          //     position: point,
-          //     label: parkId
-          //   });
-          //   marker.addListener('click', function() {
-          //     infoWindow.setContent('<div><strong>' + name + '</strong><br>' +
-          //       address + '<br>' + 'Rating: ' + rating + '</div>');
-          //     infoWindow.open(map, marker);
-          //   });
-          // })
-        // })
+          var pos = {
+            lat: parseFloat(dbPark.park_lat),
+            lng: parseFloat(dbPark.park_lon)
+          };
+          map.setCenter(pos);
+          map.setZoom(15);
+          
+          
+          dbPark.Trails.forEach(function(trail) {
+            
+            trail.Tracks.forEach(function(track) {
+              // var trailLength = [];
+              var cordArray = [];
+              track.Cords.forEach(function(cords) {
+                cordArray.push({lat: parseFloat(cords.lon), lng: parseFloat(cords.lat)});
+                // trailLength.push(cords.slice(-1)[0]);
+              })
+              console.log("cordArray lat: " + cordArray[0].lat);
+              console.log("cordArray lng: " + cordArray[0].lng);
+              
+              var trailPath = new google.maps.Polyline({
+                path: cordArray,
+                geodesic: true,
+                strokeColor: '#FF0000',
+                strokeOpacity: 1.0,
+                strokeWeight: 2
+              });
+              trailPath.setMap(map);
+              })
+              var trailId = trail.trail_id.toString();
+              var name = trail.trail_name;
+              var rating = trail.trail_rating;
+              var point = new google.maps.LatLng(
+                parseFloat(trail.Tracks[0].Cords[0].lon),
+                parseFloat(trail.Tracks[0].Cords[0].lat));
+              var marker = new google.maps.Marker({
+                map: map,
+                position: point,
+                label: trailId
+              });
+              marker.addListener('click', function() {
+                infoWindow.setContent('<div class="trail-click" id="' + trailId + '">' + '<span class="bold-font">' + name + '</span>' + '<br>' + 
+                'Rating: ' + rating + '</div>');
+                infoWindow.open(map, marker);
+              });
+            })
+          })
+          
       }
       trailMarkers(); 
 
